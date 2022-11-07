4a9e7626957d2424d9d6d9a56f614c45c4bdc191
Aakash Dubey <skydubey01@gmail.com>
2022-11-07 11:01:14 +0530

    done

--- stocks2.js
+++ stocks2.js
@@ -41,7 +41,7 @@ document.getElementById("clicktofocus").addEventListener("click", () => {
     document.getElementById("inputsearch").focus();
 });
 document.getElementById("getbtn").addEventListener("click", () => {
-    document.getElementById("loader").hide();
+    document.getElementById("loader").addClass('d-none');
 });
 
 
